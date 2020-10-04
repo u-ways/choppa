7fc61aced7839d6122e51ebb9d97173c6ac3db2a
u-ways <ue95.career@gmail.com>
2020-10-04 23:56:57 +0100

    Prepare backend business logic structure - Uways

--- backend/src/middlewares/static_server.ts
+++ backend/src/middlewares/static_server.ts
@@ -4,7 +4,7 @@ import type {Context} from "../../deps.ts";
 
 export const staticRestServer = async (ctx: Context, next: Function) => {
     await ctx.send({
-        root: `../chopper-frontend/dist`,
+        root: `../frontend/dist`,
         index: "index.html",
     });
     await next();

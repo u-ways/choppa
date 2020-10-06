592352007a0b61a2afbb1784150fc4020fc86b94
u-ways <ue95.career@gmail.com>
2020-10-06 22:04:46 +0100

    Refactor Rhum test dependency - Uways

--- backend/tests/deps.ts
+++ backend/tests/deps.ts
@@ -1,4 +1,10 @@
 /** Backend test dependencies */
-export { Rhum } from "https://deno.land/x/rhum@v1.1.2/mod.ts";
+import { Rhum } from "https://deno.land/x/rhum@v1.1.2/mod.ts";
 
+const plan = (a: string, b: Function) => Rhum.testPlan(a, b)
+const suite = (a: string, b: Function) => Rhum.testSuite(a, b)
+const scenario = (a: string, b: Function) => Rhum.testCase(a, b)
+const assertEquals = (a: any, b: any) => Rhum.asserts.assertEquals(a, b)
+const runTests = () => Rhum.run()
 
+export { plan, suite, scenario, assertEquals, runTests }

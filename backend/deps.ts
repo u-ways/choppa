7b63541d2e793a5396b151bc75c4c05880429375
u-ways <ue95.career@gmail.com>
2020-10-09 02:55:01 +0100

    Add Denon rollback #84 u-ways

--- backend/deps.ts
+++ backend/deps.ts
@@ -1,7 +1,11 @@
 /** Backend dependencies */
 export {red, blue, gray, green, cyan, bold, yellow} from "https://deno.land/std@0.69.0/fmt/colors.ts";
-export {Application, Context, Router, RouterContext, helpers, HttpError, Status} from "https://deno.land/x/oak@v6.2.0/mod.ts";
+export {
+    Application, Context, Router, RouterContext, helpers, HttpError, Status
+} from "https://deno.land/x/oak@v6.2.0/mod.ts";
 export {Pool} from "https://deno.land/x/postgres@v0.4.5/mod.ts";
 export {PoolClient} from "https://deno.land/x/postgres@v0.4.5/client.ts";
 export {applyGraphQL, gql, GQLError, ResolversProps} from "https://deno.land/x/oak_graphql@0.6.2/mod.ts"
 export {resolve, dirname, fromFileUrl} from "https://deno.land/std@0.73.0/path/mod.ts";
+export {Migration, ClientPostgreSQL} from "https://deno.land/x/nessie@v1.1.2/mod.ts";
+export * as Dex from "https://raw.githubusercontent.com/choppa-org/dex/1.0.3/mod.ts";

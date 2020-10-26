c6075ad3e83c352aec301cb9dbbd4c66848ef01f
u-ways <ue95.career@gmail.com>
2020-10-26 00:15:53 +0000

    Move backend to root directory #96 - Brett, Adam, Marin, Uways

--- frontend/src/utilities/Environment.ts
+++
@@ -1,7 +0,0 @@
-export class Environment {
-  private static readonly DEV_ENV: string = "development";
-
-  public static isDevelopment(): boolean {
-    return process.env.NODE_ENV === this.DEV_ENV;
-  }
-}

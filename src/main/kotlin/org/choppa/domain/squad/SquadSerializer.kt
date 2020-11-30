1dd040361ee25ea775ee90ba9a47147afe8ae3ac
u-ways <ue95.career@gmail.com>
2020-11-30 03:42:59 +0000

    Create BaseSerializer to refactor out queryComponent duplication #124 Uways

--- src/main/kotlin/org/choppa/domain/squad/SquadSerializer.kt
+++ src/main/kotlin/org/choppa/domain/squad/SquadSerializer.kt
@@ -2,21 +2,17 @@ package org.choppa.domain.squad
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import org.choppa.domain.chapter.ChapterController
-import org.choppa.domain.history.HistoryController
-import org.choppa.domain.iteration.IterationController
+import org.choppa.domain.base.BaseSerializer
+import org.choppa.domain.base.BaseSerializer.QueryType.CHAPTERS
+import org.choppa.domain.base.BaseSerializer.QueryType.HISTORY
+import org.choppa.domain.base.BaseSerializer.QueryType.ITERATIONS
 import org.choppa.domain.member.MemberController
 import org.choppa.domain.tribe.TribeController
 import org.choppa.utils.Color.Companion.toRGBAHex
-import org.choppa.utils.ReverseRouter
-import org.springframework.beans.factory.annotation.Autowired
 
 class SquadSerializer(
-    supportedClass: Class<Squad>? = null,
-    @Autowired private val reverseRouter: ReverseRouter = ReverseRouter(),
-) : StdSerializer<Squad>(supportedClass) {
-
+    supportedClass: Class<Squad>? = null
+) : BaseSerializer<Squad>(supportedClass) {
     override fun serialize(squad: Squad, gen: JsonGenerator, provider: SerializerProvider) {
         gen.writeStartObject()
         gen.writeStringField("id", reverseRouter.route(SquadController::class, squad.id))
@@ -26,18 +22,9 @@ class SquadSerializer(
         gen.writeArrayFieldStart("members")
         squad.members.forEach { gen.writeString(reverseRouter.route(MemberController::class, it.id)) }
         gen.writeEndArray()
-        gen.writeStringField(
-            "chapters",
-            reverseRouter.queryComponent(ChapterController::class, ChapterController::listChapters, squad)
-        )
-        gen.writeStringField(
-            "iterations",
-            reverseRouter.queryComponent(IterationController::class, IterationController::listIterations, squad)
-        )
-        gen.writeStringField(
-            "history",
-            reverseRouter.queryComponent(HistoryController::class, HistoryController::listHistory, squad)
-        )
+        gen.queryComponent(CHAPTERS, squad)
+        gen.queryComponent(ITERATIONS, squad)
+        gen.queryComponent(HISTORY, squad)
         gen.writeEndObject()
     }
 }
--- src/main/kotlin/org/choppa/domain/tribe/TribeSerializer.kt
+++ src/main/kotlin/org/choppa/domain/tribe/TribeSerializer.kt
@@ -2,46 +2,27 @@ package org.choppa.domain.tribe
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
-import org.choppa.domain.chapter.ChapterController
-import org.choppa.domain.history.HistoryController
-import org.choppa.domain.iteration.IterationController
-import org.choppa.domain.member.MemberController
-import org.choppa.domain.squad.SquadController
+import org.choppa.domain.base.BaseSerializer
+import org.choppa.domain.base.BaseSerializer.QueryType.CHAPTERS
+import org.choppa.domain.base.BaseSerializer.QueryType.HISTORY
+import org.choppa.domain.base.BaseSerializer.QueryType.ITERATIONS
+import org.choppa.domain.base.BaseSerializer.QueryType.MEMBERS
+import org.choppa.domain.base.BaseSerializer.QueryType.SQUADS
 import org.choppa.utils.Color.Companion.toRGBAHex
-import org.choppa.utils.ReverseRouter
-import org.springframework.beans.factory.annotation.Autowired
 
 class TribeSerializer(
-    supportedClass: Class<Tribe>? = null,
-    @Autowired private val reverseRouter: ReverseRouter = ReverseRouter(),
-) : StdSerializer<Tribe>(supportedClass) {
-
+    supportedClass: Class<Tribe>? = null
+) : BaseSerializer<Tribe>(supportedClass) {
     override fun serialize(tribe: Tribe, gen: JsonGenerator, provider: SerializerProvider) {
         gen.writeStartObject()
         gen.writeStringField("id", reverseRouter.route(TribeController::class, tribe.id))
         gen.writeStringField("name", tribe.name)
         gen.writeStringField("color", tribe.color.toRGBAHex())
-        gen.writeStringField(
-            "chapters",
-            reverseRouter.queryComponent(ChapterController::class, ChapterController::listChapters, tribe)
-        )
-        gen.writeStringField(
-            "members",
-            reverseRouter.queryComponent(MemberController::class, MemberController::listMembers, tribe)
-        )
-        gen.writeStringField(
-            "squads",
-            reverseRouter.queryComponent(SquadController::class, SquadController::listSquads, tribe)
-        )
-        gen.writeStringField(
-            "iterations",
-            reverseRouter.queryComponent(IterationController::class, IterationController::listIterations, tribe)
-        )
-        gen.writeStringField(
-            "history",
-            reverseRouter.queryComponent(HistoryController::class, HistoryController::listHistory, tribe)
-        )
+        gen.queryComponent(CHAPTERS, tribe)
+        gen.queryComponent(MEMBERS, tribe)
+        gen.queryComponent(SQUADS, tribe)
+        gen.queryComponent(ITERATIONS, tribe)
+        gen.queryComponent(HISTORY, tribe)
         gen.writeEndObject()
     }
 }

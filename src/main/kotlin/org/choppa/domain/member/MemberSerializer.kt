7ecc96c579531344e569dfa0b29717fce375e864
u-ways <ue95.career@gmail.com>
2020-11-30 03:42:59 +0000

    Create BaseSerializer to refactor out queryComponent duplication #124 Uways

--- src/main/kotlin/org/choppa/domain/member/MemberSerializer.kt
+++ src/main/kotlin/org/choppa/domain/member/MemberSerializer.kt
@@ -2,36 +2,23 @@ package org.choppa.domain.member
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import org.choppa.domain.base.BaseSerializer
+import org.choppa.domain.base.BaseSerializer.QueryType.HISTORY
+import org.choppa.domain.base.BaseSerializer.QueryType.ITERATIONS
+import org.choppa.domain.base.BaseSerializer.QueryType.SQUADS
 import org.choppa.domain.chapter.ChapterController
-import org.choppa.domain.history.HistoryController
-import org.choppa.domain.iteration.IterationController
-import org.choppa.domain.squad.SquadController
-import org.choppa.utils.ReverseRouter
-import org.springframework.beans.factory.annotation.Autowired
 
 class MemberSerializer(
-    supportedClass: Class<Member>? = null,
-    @Autowired private val reverseRouter: ReverseRouter = ReverseRouter(),
-) : StdSerializer<Member>(supportedClass) {
-
+    supportedClass: Class<Member>? = null
+) : BaseSerializer<Member>(supportedClass) {
     override fun serialize(member: Member, gen: JsonGenerator, provider: SerializerProvider) {
         gen.writeStartObject()
         gen.writeStringField("id", reverseRouter.route(MemberController::class, member.id))
         gen.writeStringField("name", member.name)
         gen.writeStringField("chapter", reverseRouter.route(ChapterController::class, member.chapter.id))
-        gen.writeStringField(
-            "squads",
-            reverseRouter.queryComponent(SquadController::class, SquadController::listSquads, member)
-        )
-        gen.writeStringField(
-            "iterations",
-            reverseRouter.queryComponent(IterationController::class, IterationController::listIterations, member)
-        )
-        gen.writeStringField(
-            "history",
-            reverseRouter.queryComponent(HistoryController::class, HistoryController::listHistory, member)
-        )
+        gen.queryComponent(SQUADS, member)
+        gen.queryComponent(ITERATIONS, member)
+        gen.queryComponent(HISTORY, member)
         gen.writeEndObject()
     }
 }

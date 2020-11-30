a6d98e5c18a04706bd722e8d7b34cd2d591dd4a9
Brett Taylor <bretttaylor1998@live.co.uk>
2020-11-30 00:52:12 +0000

    Split out stores to separate stores. #141 - Brett

--- src/frontend/src/config/store/store.js
+++ src/frontend/src/config/store/store.js
@@ -1,44 +1,28 @@
-/* eslint-disable no-shadow, no-param-reassign */
 import Vue from "vue";
 import Vuex from "vuex";
-import { isValidThemeSetting, themeSetting } from "@/enums/themeSetting";
-import { UPDATE_CURRENT_THEME, UPDATE_PREFERRED_THEME } from "@/config/store/mutation-types";
+import {
+  darkModeActions,
+  darkModeGetters,
+  darkModeMutations,
+  darkModeState,
+} from "@/config/store/stores/darkmode.store";
 
 Vue.use(Vuex);
 
 const state = {
-  currentTheme: undefined,
-  preferredTheme: localStorage.preferredTheme,
+  ...darkModeState,
 };
 
 const getters = {
-  currentTheme: (state) => (state.currentTheme === undefined ? themeSetting.DARK_THEME : state.currentTheme),
-  preferredTheme: (state) => (state.preferredTheme === undefined ? themeSetting.FOLLOW_OS : state.preferredTheme),
+  ...darkModeGetters,
 };
 
 const mutations = {
-  [UPDATE_CURRENT_THEME](state, newTheme) {
-    if (isValidThemeSetting(newTheme) && newTheme !== themeSetting.FOLLOW_OS) {
-      state.currentTheme = newTheme;
-    } else {
-      throw new Error(`Unsupported value ${newTheme}`);
-    }
-  },
-  [UPDATE_PREFERRED_THEME](state, newTheme) {
-    if (isValidThemeSetting(newTheme)) {
-      state.preferredTheme = newTheme;
-      localStorage.setItem("preferredTheme", newTheme);
-    } else {
-      throw new Error(`Unsupported value ${newTheme}`);
-    }
-  },
+  ...darkModeMutations,
 };
 
 const actions = {
-  updateTheme(context, newTheme) {
-    context.commit(UPDATE_CURRENT_THEME, newTheme);
-    context.commit(UPDATE_PREFERRED_THEME, newTheme);
-  },
+  ...darkModeActions,
 };
 
 export default new Vuex.Store({

45b2a96eea98a48fcac971bee562e8bc1e134597
u-ways <ue95.career@gmail.com>
2020-12-02 00:48:52 +0000

    Add Squad collection PUT/POST/DELETE endpoints #163 Uways

--- src/main/kotlin/app/choppa/domain/squad/SquadController.kt
+++ src/main/kotlin/app/choppa/domain/squad/SquadController.kt
@@ -7,8 +7,12 @@ import app.choppa.domain.tribe.Tribe
 import app.choppa.utils.QueryComponent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.http.ResponseEntity.ok
+import org.springframework.http.ResponseEntity.*
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -30,4 +34,21 @@ class SquadController(
             else -> squadService.find()
         }
     )
+
+    @PutMapping
+    fun putCollection(@RequestBody updatedCollection: List<Squad>): ResponseEntity<List<Squad>> = squadService
+        .find(updatedCollection.map { it.id })
+        .also { squadService.save(updatedCollection) }
+        .run { created(location()).build() }
+
+    @DeleteMapping
+    fun deleteCollection(@RequestBody toDeleteCollection: List<Squad>): ResponseEntity<List<Squad>> = squadService
+        .find(toDeleteCollection.map { it.id })
+        .also { squadService.delete(toDeleteCollection) }
+        .run { noContent().build() }
+
+    @PostMapping
+    fun postCollection(@RequestBody newCollection: List<Squad>): ResponseEntity<List<Squad>> = squadService
+        .save(newCollection)
+        .run { created(location()).build() }
 }
---
+++ src/test/kotlin/app/choppa/integration/domain/squad/SquadControllerCollectionIT.kt
@@ -0,0 +1,122 @@
+package app.choppa.integration.domain.squad
+
+import app.choppa.domain.squad.Squad
+import app.choppa.domain.squad.SquadController
+import app.choppa.domain.squad.SquadService
+import app.choppa.exception.EmptyListException
+import app.choppa.support.factory.SquadFactory
+import app.choppa.utils.Color.Companion.toRGBAInt
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.hamcrest.core.StringContains
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders.LOCATION
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.delete
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
+
+@WebMvcTest(controllers = [SquadController::class])
+@ActiveProfiles("test")
+internal class SquadControllerCollectionIT @Autowired constructor(
+    private val mvc: MockMvc,
+    private val mapper: ObjectMapper,
+) {
+
+    @MockkBean
+    private lateinit var squadService: SquadService
+
+    @Nested
+    inner class HappyPath {
+
+        @Test
+        fun `LIST entities`() {
+            val entities = SquadFactory.create(amount = 2)
+
+            every { squadService.find() } returns entities
+
+            mvc.get("/api/squads") {
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+            }.andExpect {
+                status { isOk }
+                content { contentType(APPLICATION_JSON) }
+                content { json(mapper.writeValueAsString(entities)) }
+            }
+        }
+
+        @Test
+        fun `PUT collection`() {
+            val existingCollection = SquadFactory.create(amount = 3)
+            val greenColor = "#00FF00".toRGBAInt()
+            val updatedCollection = existingCollection.map { Squad(it.id, it.name, greenColor) }
+
+            every { squadService.find(existingCollection.map { it.id }) } returns existingCollection
+            every { squadService.save(updatedCollection) } returns updatedCollection
+
+            mvc.put("/api/squads") {
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+                content = mapper.writeValueAsString(updatedCollection)
+            }.andExpect {
+                status { isCreated }
+                header { string(LOCATION, StringContains("/api/squads")) }
+            }
+        }
+
+        @Test
+        fun `DELETE collection`() {
+            val existingCollection = SquadFactory.create(amount = 3)
+
+            every { squadService.find(existingCollection.map { it.id }) } returns existingCollection
+            every { squadService.delete(existingCollection) } returns existingCollection
+
+            mvc.delete("/api/squads") {
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+                content = mapper.writeValueAsString(existingCollection)
+            }.andExpect {
+                status { isNoContent }
+            }
+        }
+
+        @Test
+        fun `POST collection`() {
+            val newCollection = SquadFactory.create(amount = 3)
+
+            every { squadService.save(newCollection) } returns newCollection
+
+            mvc.post("/api/squads") {
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+                content = mapper.writeValueAsString(newCollection)
+            }.andExpect {
+                status { isCreated }
+                header { string(LOCATION, StringContains("/api/squads")) }
+            }
+        }
+    }
+
+    @Nested
+    inner class SadPath {
+
+        @Test
+        fun `LIST no content`() {
+            every { squadService.find() } throws EmptyListException("No squads exist yet")
+
+            mvc.get("/api/squads") {
+                contentType = APPLICATION_JSON
+                accept = APPLICATION_JSON
+            }.andExpect {
+                status { isNoContent }
+            }
+        }
+    }
+}

80170c7313bc573b8cb9476cbbe64ef20c251b34
u-ways <ue95.career@gmail.com>
2020-12-02 00:47:57 +0000

    Add Chapters collection PUT/POST/DELETE endpoints #163 Uways

--- src/main/kotlin/app/choppa/domain/chapter/ChapterController.kt
+++ src/main/kotlin/app/choppa/domain/chapter/ChapterController.kt
@@ -7,8 +7,14 @@ import app.choppa.domain.tribe.Tribe
 import app.choppa.utils.QueryComponent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.created
+import org.springframework.http.ResponseEntity.noContent
 import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -30,4 +36,21 @@ class ChapterController(
             else -> chapterService.find()
         }
     )
+
+    @PutMapping
+    fun putCollection(@RequestBody updatedCollection: List<Chapter>): ResponseEntity<List<Chapter>> = chapterService
+        .find(updatedCollection.map { it.id })
+        .also { chapterService.save(updatedCollection) }
+        .run { created(location()).build() }
+
+    @DeleteMapping
+    fun deleteCollection(@RequestBody toDeleteCollection: List<Chapter>): ResponseEntity<List<Chapter>> = chapterService
+        .find(toDeleteCollection.map { it.id })
+        .also { chapterService.delete(toDeleteCollection) }
+        .run { noContent().build() }
+
+    @PostMapping
+    fun postCollection(@RequestBody newCollection: List<Chapter>): ResponseEntity<List<Chapter>> = chapterService
+        .save(newCollection)
+        .run { created(location()).build() }
 }

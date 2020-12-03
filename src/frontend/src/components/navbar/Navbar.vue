ee0f9568e3b128055a6611ef144fa59313f4e842
Brett Taylor <bretttaylor1998@live.co.uk>
2020-12-03 01:33:09 +0000

    Navbar on mobile devices now has a z index to appear in front of body content #141 - Brett

--- src/frontend/src/components/navbar/Navbar.vue
+++ src/frontend/src/components/navbar/Navbar.vue
@@ -5,12 +5,13 @@
         <IconButton class="sm:hidden hover:bg-gray-200 dark:hover:bg-gray-700"
                     screenReaderText="Open Menu" icon="bars" @click="expandedMenu = true"
         />
-        <div class="absolute top-0 left-0 w-full sm:relative sm:block" :class="expandedMenu ? '' : 'hidden'">
+        <div :class="expandedMenu ? '' : 'hidden'" class="absolute top-0 left-0 w-full sm:relative sm:block z-10
+        sm:z-auto">
           <div class="mx-2 mt-2 p-5 pt-5 rounded-lg sm:rounded-none shadow-lg ring-1 ring-black ring-opacity-5 bg-white
           dark:bg-gray-700 divide-gray-50 sm:flex sm:flex-row sm:gap-4 sm:shadow-none sm:ring-0 sm:bg-transparent
           sm:dark:bg-transparent sm:m-0 sm:p-0 sm:ml-4">
             <div class="flex justify-between mb-2 sm:hidden">
-              <ChoppaLogo css="text-3xl text-gray-700 dark:text-gray-300"></ChoppaLogo>
+              <ChoppaLogo css="text-2xl text-gray-700 dark:text-gray-300"></ChoppaLogo>
               <IconButton class="sm:hidden hover:bg-gray-200 dark:hover:bg-gray-800"
                           screenReaderText="Close Menu" icon="times" @click="expandedMenu = false"
               />

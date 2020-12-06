3b73248516f423fa63054a43d58ce0826d5294c0
MD485 <md485@kent.ac.uk>
2020-12-06 13:40:54 +0000

    Renamed Options to RotationOptions, added a RotationService.kt, added a rotation endpoint to the TribeController.kt, fixed history shared reference when creating a new tribe in the rotation method.

--- src/main/kotlin/app/choppa/core/rotation/RotationOptions.kt
+++ src/main/kotlin/app/choppa/core/rotation/RotationOptions.kt
@@ -6,13 +6,13 @@ import app.choppa.core.rotation.strategy.Strategy
 import app.choppa.core.rotation.strategy.Strategy.CLOCKWISE
 import app.choppa.domain.chapter.Chapter
 
-data class Options(
+data class RotationOptions(
     val amount: Int = 1,
     val chapter: Chapter = Chapter.UNASSIGNED_ROLE,
     val filter: Filter = OLDEST,
     val strategy: Strategy = CLOCKWISE
 ) {
     companion object {
-        val DEFAULT_OPTIONS = Options()
+        val DEFAULT_OPTIONS = RotationOptions()
     }
 }

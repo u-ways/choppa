c8760de5868e4510b028fe77cee8f0b949a2fee9
u-ways <ue95.career@gmail.com>
2020-12-08 12:09:01 +0000

    Add Chapter_id to History table #166 Uways

--- src/main/resources/db/migration/V7_0_0__history_relation.sql
+++ src/main/resources/db/migration/V7_0_0__history_relation.sql
@@ -1,14 +1,17 @@
 CREATE TABLE history
 (
-    iteration_id UUID NOT NULL,
-    tribe_id     UUID NOT NULL,
-    squad_id     UUID NOT NULL,
-    member_id    UUID NOT NULL,
+    iteration_id UUID,
+    tribe_id     UUID,
+    squad_id     UUID,
+    member_id    UUID,
+    chapter_id   UUID,
     create_date  TIMESTAMP WITH TIME ZONE NOT NULL,
 
-    PRIMARY KEY (iteration_id, tribe_id, squad_id, member_id, create_date),
+    PRIMARY KEY (iteration_id, tribe_id, squad_id, member_id, chapter_id, create_date),
     CONSTRAINT history_fk1 FOREIGN KEY (iteration_id) REFERENCES iteration (iteration_id),
     CONSTRAINT history_fk2 FOREIGN KEY (tribe_id) REFERENCES tribe (tribe_id),
     CONSTRAINT history_fk3 FOREIGN KEY (squad_id) REFERENCES squad (squad_id),
-    CONSTRAINT history_fk4 FOREIGN KEY (member_id) REFERENCES member (member_id)
+    CONSTRAINT history_fk5 FOREIGN KEY (member_id) REFERENCES member (member_id),
+    CONSTRAINT history_fk6 FOREIGN KEY (chapter_id) REFERENCES chapter (chapter_id)
 );
+

2b2a683bbaf1d130d3018ea90d8c9f073f53ca4a
u-ways <ue95.career@gmail.com>
2021-01-26 20:36:15 +0000

    Refactor out History model from Iteration #175 U-ways

--- src/main/kotlin/app/choppa/domain/iteration/IterationController.kt
+++ src/main/kotlin/app/choppa/domain/iteration/IterationController.kt
@@ -9,15 +9,8 @@ import app.choppa.utils.QueryComponent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.*
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("$API_PREFIX/iterations")
@@ -29,14 +22,7 @@ class IterationController(
         @QueryComponent(Member::class) @RequestParam(name = "member", required = false) memberId: UUID?,
         @QueryComponent(Squad::class) @RequestParam(name = "squad", required = false) squadId: UUID?,
         @QueryComponent(Tribe::class) @RequestParam(name = "tribe", required = false) tribeId: UUID?,
-    ): ResponseEntity<List<Iteration>> = ok().body(
-        when {
-            tribeId is UUID -> iterationService.findRelatedByTribe(tribeId)
-            squadId is UUID -> iterationService.findRelatedBySquad(squadId)
-            memberId is UUID -> iterationService.findRelatedByMember(memberId)
-            else -> iterationService.find()
-        }
-    )
+    ): ResponseEntity<List<Iteration>> = ok().body(iterationService.find())
 
     @PutMapping
     fun putCollection(@RequestBody updatedCollection: List<Iteration>): ResponseEntity<List<Iteration>> = iterationService

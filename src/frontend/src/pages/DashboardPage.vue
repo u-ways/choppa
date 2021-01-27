9e9a5ddb0b2317052d12898076bcb868328747ad
Brett Taylor <bretttaylor1998@live.co.uk>
2021-01-27 00:39:11 +0000

    Added DashboardPage, replaced all existing links on the navbar with a single "dashboard" link. #178 - Brett

---
+++ src/frontend/src/pages/DashboardPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <StandardPageTemplate>
+      <template v-slot:page-header>
+        <div class="text-3xl font-bold">
+          Dashboard
+        </div>
+      </template>
+    </StandardPageTemplate>
+  </div>
+</template>
+
+<script>
+import StandardPageTemplate from "@/components/templates/StandardPageTemplate";
+
+export default {
+  name: "Dashboard",
+  components: {
+    StandardPageTemplate,
+  },
+};
+</script>

aa82bd3bbafaf25ba49fb5156fdbeb1d05006ef7
u-ways <ue95.career@gmail.com>
2021-01-28 13:01:00 +0000

    Fix UNASSIGNED_ROLE alias typo #165 U-ways

--- src/main/kotlin/app/choppa/domain/rotation/RotationOptions.kt
+++ src/main/kotlin/app/choppa/domain/rotation/RotationOptions.kt
@@ -1,15 +1,15 @@
 package app.choppa.domain.rotation
 
 import app.choppa.domain.chapter.Chapter
+import app.choppa.domain.chapter.Chapter.Companion.UNASSIGNED_ROLE
 import app.choppa.domain.rotation.filter.Filter
 import app.choppa.domain.rotation.filter.Filter.OLDEST
 import app.choppa.domain.rotation.strategy.Strategy
 import app.choppa.domain.rotation.strategy.Strategy.CLOCKWISE
-import app.choppa.domain.chapter.Chapter.Companion.UNASSIGNED_ROLE as UNASSIGNED_ROLE1
 
 data class RotationOptions(
     val amount: Int = 1,
-    val chapter: Chapter = UNASSIGNED_ROLE1,
+    val chapter: Chapter = UNASSIGNED_ROLE,
     val filter: Filter = OLDEST,
     val strategy: Strategy = CLOCKWISE
 ) {

78b46937f42841fe69e49e81271a4590e7dbe41a
u-ways <ue95.career@gmail.com>
2021-02-12 22:39:52 +0000

    Refactor strategy to remove side-effects. #181 u-ways

--- src/main/kotlin/app/choppa/domain/rotation/strategy/antiClockwise.kt
+++ src/main/kotlin/app/choppa/domain/rotation/strategy/antiClockwise.kt
@@ -1,9 +1,6 @@
 package app.choppa.domain.rotation.strategy
 
 import app.choppa.domain.member.Member
-import java.util.*
 
-internal fun antiClockwise(members: List<List<Member>>): List<List<Member>> = members.apply {
-    // TODO(u-ways) #52 maybe implement our own left shift if this have bad performance?
-    Collections.rotate(this, -1)
-}
+internal fun antiClockwise(members: List<List<Member>>): List<List<Member>> =
+    members.drop(1) + members.take(1)

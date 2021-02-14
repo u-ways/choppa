bd218595ab5ce7a30a4a06451aa115a03fdb8456
Brett Taylor <bretttaylor1998@live.co.uk>
2021-02-14 21:38:28 +0000

    No longer check authentication status every time the use goes to a page that requires authentication. Instead an axios interceptor will listen for 401s and if one is received, the user will be logged out. #186 - Brett

--- src/frontend/src/config/api/http-client.js
+++ src/frontend/src/config/api/http-client.js
@@ -1,10 +1,19 @@
 import axios from "axios";
+import Vue from "vue";
 
 const httpClient = axios.create({
   baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+httpClient.interceptors.response.use(undefined, async (error) => {
+  if (error.response.status === 401) {
+    await Vue.prototype.$store.dispatch("logOut");
+  }
+
+  return Promise.reject(error);
 });
 
 export default httpClient;

0d0b4f5a7d859542732044bfd29b82c53464ccf0
Brett Taylor <bretttaylor1998@live.co.uk>
2021-02-15 00:53:59 +0000

    New navigation bar #186 - Brett

--- src/frontend/src/components/navbar/Navbar.vue
+++ src/frontend/src/components/navbar/Navbar.vue
@@ -1,61 +1,62 @@
 <template>
-    <nav class="py-3">
-      <div class="px-5 flex justify-between sm:justify-start">
-        <NavbarLink :url="{ name: 'home' }" css="sm:font-bold sm:text-lg">Choppa.app</NavbarLink>
-        <IconButton class="sm:hidden hover:bg-gray-200 dark:hover:bg-gray-700"
-                    screenReaderText="Open Menu" icon="bars" @click="expandedMenu = true"
-        />
-        <div :class="expandedMenu ? '' : 'hidden'" class="absolute top-0 left-0 w-full sm:relative sm:block z-10
-        sm:z-auto">
-          <div class="mx-2 mt-2 p-5 pt-5 rounded-lg sm:rounded-none shadow-lg ring-1 ring-black ring-opacity-5 bg-white
-          dark:bg-gray-700 divide-gray-50 sm:flex sm:flex-row sm:gap-4 sm:shadow-none sm:ring-0 sm:bg-transparent
-          sm:dark:bg-transparent sm:m-0 sm:p-0 sm:ml-4">
-            <div class="flex justify-between mb-2 sm:hidden">
-              <ChoppaLogo css="text-2xl text-gray-700 dark:text-gray-300"></ChoppaLogo>
-              <IconButton class="sm:hidden hover:bg-gray-200 dark:hover:bg-gray-800"
-                          screenReaderText="Close Menu" icon="times" @click="expandedMenu = false"
-              />
-            </div>
-            <div class="flex-grow flex flex-row gap-1">
-              <NavbarLink :url="{ name: 'dashboard' }" v-if="isAuthenticated">Dashboard</NavbarLink>
-            </div>
-            <div class="flex-grow flex flex-row gap-3 sm:justify-end">
-              <template v-if="isAuthenticated">
-                <div class="font-lg leading-10">
-                  Hello, <span class="font-bold">{{authenticatedAccount.name}}</span>
-                  <button class="bg-red-500" @click="onLogOutClicked">Logout</button>
-                </div>
-              </template>
-              <template v-else>
-                <NavbarLink :url="{ name: 'login' }">Login</NavbarLink>
-                <router-link :to="{ name: 'login' }"
-                           class="px-2 font-bold rounded-md leading-10 text-sm duration-200 outline-none ring-0
+    <nav class="p-3 flex justify-between">
+      <div class="flex flex-row gap-4">
+        <NavbarLink :url="{ name: 'home' }" :is-on-home-page="isOnHomePage" css="font-bold sm:text-lg">
+          Choppa.app
+        </NavbarLink>
+        <NavbarLink :url="{ name: 'dashboard' }" :is-on-home-page="isOnHomePage" v-if="isAuthenticated">
+          Dashboard
+        </NavbarLink>
+      </div>
+      <div class="flex-grow flex flex-row gap-3 justify-end items-center">
+        <template v-if="isAuthenticated">
+          <button class="flex flex-row gap-2 items-center outline-none ring-0 focus:outline-none"
+                  @click="expandedMenu = !expandedMenu">
+            <span class="hidden sm:block font-bold text-sm"
+                  :class="[ isOnHomePage ? 'text-gray-200' : 'text-gray-800 dark:text-gray-200' ]">
+              {{authenticatedAccount.shortName }}
+            </span>
+            <Avatar class="w-8 h-8"
+                    :class="[ expandedMenu === true ? 'border-2 border-choppa-two' : '' ]"
+                    :seed="authenticatedAccount.avatarSeed"
+            />
+          </button>
+        </template>
+        <template v-else>
+          <NavbarLink :url="{ name: 'login' }" :is-on-home-page="isOnHomePage">Login</NavbarLink>
+          <router-link :to="{ name: 'login' }"
+                       class="px-2 font-bold rounded-md leading-10 text-sm duration-200 outline-none ring-0
                            focus:ring-4 transform-gpu transition-transform transition-colors hover:-translate-y-0.5
                            focus:-translate-y-0.5 duration-100 motion-reduce:transition-none"
-                             :class="isOnHomePage === true
+                       :class="isOnHomePage === true
                            ? [ 'bg-white', 'text-gray-600', 'ring-choppa-light-extra', 'hover:bg-gray-200' ]
                            : [ 'bg-choppa-two', 'text-gray-100', 'ring-indigo-500', 'hover:bg-indigo-500']"
-                >
-                  Try Now
-                </router-link>
-              </template>
-            </div>
-          </div>
-        </div>
+          >
+            Try Now
+          </router-link>
+        </template>
+      </div>
+      <div v-if="isAuthenticated" v-show="expandedMenu">
+        <NavbarAccountMenu @hide="expandedMenu = false" :is-on-home-page="isOnHomePage" />
       </div>
     </nav>
 </template>
 
 <script>
+/* eslint-disable */
 import NavbarLink from "@/components/navbar/NavbarLink";
 import ChoppaLogo from "@/components/atoms/ChoppaLogo";
 import IconButton from "@/components/atoms/buttons/IconButton";
 import { mapActions, mapGetters } from "vuex";
 import router from "@/config/router";
+import Avatar from "@/components/member/Avatar";
+import NavbarAccountMenu from "@/components/navbar/NavbarAccountMenu";
 
 export default {
   name: "Navbar",
   components: {
+    NavbarAccountMenu,
+    Avatar,
     IconButton,
     ChoppaLogo,
     NavbarLink,
@@ -74,13 +75,6 @@ export default {
     return {
       expandedMenu: false,
     };
-  },
-  methods: {
-    ...mapActions(["logOut"]),
-    async onLogOutClicked() {
-      await this.logOut();
-      await router.push({ name: "home" });
-    },
-  },
+  }
 };
 </script>
---
+++ src/frontend/src/components/navbar/NavbarAccountMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 z-10">
+    <div class="left-0 right-0 top-0 bottom-0 absolute bg-white dark:bg-gray-700">
+      <IconButton icon="times"
+                  class="absolute top-0 right-0 mr-2 mt-1 hover:bg-gray-200 dark:hover:bg-choppa-dark"
+                  screen-reader-text="Close account menu"
+                  @click="hideAccountMenu"
+      />
+      <div class="px-5 py-3 flex flex-col items-center gap-2 h-full justify-center -mt-12">
+        <Avatar class="w-20 h-20" :seed="authenticatedAccount.avatarSeed"/>
+        <div class="text-center text-sm flex flex-col dark:text-gray-200">
+          <div class="font-bold">{{authenticatedAccount.name}}</div>
+          <div>Organisation</div>
+        </div>
+        <StyledButton type="button" variant="primary" @click="onLogoutClicked">
+          Logout
+        </StyledButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import Avatar from "@/components/member/Avatar";
+import IconButton from "@/components/atoms/buttons/IconButton";
+import StyledButton from "@/components/atoms/buttons/StyledButton";
+import router from "@/config/router";
+
+export default {
+  name: "NavbarAccountMenu",
+  components: {
+    Avatar,
+    IconButton,
+    StyledButton,
+  },
+  computed: {
+    ...mapGetters(["authenticatedAccount"]),
+  },
+  methods: {
+    ...mapActions(["logOut"]),
+    hideAccountMenu() {
+      this.$emit("hide");
+    },
+    async onLogoutClicked() {
+      await this.logOut();
+
+      if (this.$route.name !== "home") {
+        await router.push({ name: "home" });
+      }
+    },
+  },
+};
+</script>

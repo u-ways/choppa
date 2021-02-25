aeaf5ca3adcead1451c139debd53a55b3548513f
u-ways <ue95.career@gmail.com>
2021-02-25 21:14:41 +0000

    Refactor Account JPA Model to match DB column definitions, Fix equals contract to primary key only. #186 U-ways

--- src/main/resources/db/migration/V8_0_0__account_domain_model.sql
+++ src/main/resources/db/migration/V8_0_0__account_domain_model.sql
@@ -1,4 +1,4 @@
-CREATE TABLE accounts
+CREATE TABLE account
 (
     account_id        UUID          PRIMARY KEY,
     provider          VARCHAR(100)  NOT NULL,
@@ -11,24 +11,24 @@ CREATE TABLE accounts
 ALTER TABLE tribe
 ADD COLUMN account_id UUID,
 ALTER COLUMN account_id SET NOT NULL,
-ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE squad
 ADD COLUMN account_id UUID,
 ALTER COLUMN account_id SET NOT NULL,
-ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE member
 ADD COLUMN account_id UUID,
 ALTER COLUMN account_id SET NOT NULL,
-ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE chapter
 ADD COLUMN account_id UUID,
 ALTER COLUMN account_id SET NOT NULL,
-ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id);
 
 ALTER TABLE iteration
 ADD COLUMN account_id UUID,
 ALTER COLUMN account_id SET NOT NULL,
-ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts (account_id);
+ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (account_id);

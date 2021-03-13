653deca688cd2269ba4368ab6d7e2101d3e52e68
u-ways <ue95.career@gmail.com>
2021-03-13 00:37:12 +0000

    Remove unused iteration feature #221 U-ways

--- src/main/kotlin/app/choppa/domain/iteration/IterationRepository.kt
+++
@@ -1,8 +0,0 @@
-package app.choppa.domain.iteration
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import java.util.*
-
-@Repository
-interface IterationRepository : JpaRepository<Iteration, UUID>

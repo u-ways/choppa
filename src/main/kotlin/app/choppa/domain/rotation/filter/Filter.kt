2f4a40d3b678a84b05dd6242b7c3425c579cdc40
u-ways <ue95.career@gmail.com>
2021-03-17 02:49:51 +0000

    Add NONE filter #1 U-ways

--- src/main/kotlin/app/choppa/domain/rotation/filter/Filter.kt
+++ src/main/kotlin/app/choppa/domain/rotation/filter/Filter.kt
@@ -3,6 +3,12 @@ package app.choppa.domain.rotation.filter
 import app.choppa.domain.member.Member
 
 enum class Filter : Policy {
+    NONE {
+        override fun invoke(
+            candidates: List<List<Member>>,
+            amount: Int
+        ): List<List<Member>> = candidates
+    },
     DISTRIBUTED {
         override fun invoke(
             candidates: List<List<Member>>,

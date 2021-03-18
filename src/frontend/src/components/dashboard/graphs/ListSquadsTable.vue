80584076df6f847f010b1b62f9db9d962fec2081
Brett Taylor <bretttaylor1998@live.co.uk>
2021-03-18 21:53:02 +0000

    Changed names in Tribes/Squad/Members overview dashboard cards to be links #227 - Brett

--- src/frontend/src/components/dashboard/graphs/ListSquadsTable.vue
+++ src/frontend/src/components/dashboard/graphs/ListSquadsTable.vue
@@ -3,7 +3,9 @@
     <div class="flex flex-row gap-1 justify-between border-l-4 border-choppa-two pl-2"
          v-for="squad in squads" :key="squad.id"
          :style="{ 'border-left-color': squad.color }">
-      <div class="font-semibold self-center">{{ squad.name }}</div>
+      <router-link :to="{ name: 'edit-squad', params: { id: squad.path } }" class="font-semibold self-center">
+        {{ squad.name }}
+      </router-link>
       <div class="flex flex-col">
         <div class="text-right">{{ squad.members.length }} Members</div>
         <div class="flex flex-row-reverse gap-1">
